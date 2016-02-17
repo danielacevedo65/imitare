e6c8335f9b7378e969091ac4ba64291fb5a66445
Priyank Patel <tocubed@gmail.com>
2016-02-17 14:59:53 -0800

    Make postagger module.

---
+++ postagger.py
@@ -0,0 +1,18 @@
+from nltk.tag import StanfordPOSTagger
+from nltk.internals import find_jars_within_path
+
+_stanford_postagger = StanfordPOSTagger('stanford-postagger/models/english-bidirectional-distsim.tagger', path_to_jar='stanford-postagger/stanford-postagger.jar')
+_stanford_postagger._stanford_jar = ':'.join(find_jars_within_path('stanford-postagger'))
+
+def stanford_postagger():
+    return _stanford_postagger
+
+if __name__ == '__main__':
+    import sys
+    tagger = stanford_postagger()
+    
+    try:
+        for line in sys.stdin:
+            print(tagger.tag(line.split()))
+    except KeyboardInterrupt:
+        pass
