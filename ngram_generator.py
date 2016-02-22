792a61b1c7edc8f584dbdca44d68c2be202b4ca0
Priyank Patel <tocubed@gmail.com>
2016-02-21 19:59:56 -0800

    Fix logical bug and add debugging output to ngram

--- ngram_generator.py
+++ ngram_generator.py
@@ -23,20 +23,21 @@ class ngram_generator:
         if len(context) == 0:
             return nltk.ELEProbDist(self._tag_to_freqdist[tag]).generate()
         else:
-            context = (model._lpad + tuple(context))[1 - self._n:]
+            context = (model._lpad + tuple(context))[1 - model._n:]
             if context in model:
                 freqdist = nltk.FreqDist()
                 for (word, freq) in model[context].freqdist().most_common():
                     if (word, tag) in self._tagged_word_kinds:
                         freqdist += nltk.FreqDist({word: freq})
                 
+                print("context with freqdist", repr(freqdist))
                 if len(freqdist) > 0:
                     return nltk.ELEProbDist(freqdist).generate()
-                else:
-                    return model[context].generate()
-            elif model._n > 1:
+            if model._n > 1:
+                print("backing off:", context)
                 return self._choose_word_by_tag(tag, model.backoff, context)
             else:
+                print("no alternative")
                 return nltk.ELEProbDist(self._tag_to_freqdist[tag]).generate()
 
     def generate(self, n):
@@ -56,7 +57,6 @@ class ngram_generator:
         for (word, _) in self._tagged_words:
             yield word
 
-
 if __name__ == '__main__':
     import sys
     from tokenizer import stanford_tokenizer
@@ -71,9 +71,16 @@ if __name__ == '__main__':
     with open(sys.argv[1]) as input_file:
         text = input_file.read()
 
-    model = ngram_generator(stanford_postagger().tag(stanford_tokenizer().tokenize(text)), int(sys.argv[2]))
+    # large text might cause an out of memory exception, let's make sure that doesn't happen
+    split_by_space = text.split(' ')
+
+    tagged = []
+    for i in range(0, len(split_by_space), 20000): # tagging 20000 words takes about 2gB of ram
+        tagged += stanford_postagger().tag(stanford_tokenizer().tokenize(' '.join(split_by_space[i:i+20000]))) 
+
+    model = ngram_generator(tagged, int(sys.argv[2]))
 
     while True:
         num_words = input("Enter the length in words to generate: ")
         if num_words.isdigit():
-            print(model.generate(int(num_words)))
+            print(' '.join(model.generate(int(num_words))))
