b13e52af7e51af1e9100dbb1a9edf0c16ea245d9
Priyank Patel <tocubed@gmail.com>
2016-02-29 15:39:41 -0800

    Add sqlite3 based dictionary for storing ngrams

--- generator.py
+++ generator.py
@@ -1,6 +1,7 @@
 from nltk.probability import ConditionalFreqDist, MLEProbDist
 from ngram import NgramModel
 
+
 class WordIdDictionary:
     """ Mapping for word <-> id transformation """
 
@@ -25,11 +26,18 @@ class WordIdDictionary:
             self._to_word[self._count] = word
             self._count += 1
 
+    def transform_word(self, word):
+        return self._to_id[word]
+    
+    def transform_id(self, id):
+        return self._to_word[id]
+
     def transform_words(self, words):
         return map(lambda word: self._to_id[word], words)
 
     def transform_ids(self, ids):
-        return map(lambda ids: self._to_word[ids], ids)
+        return map(lambda id: self._to_word[id], ids)
+
 
 class LVGNgramGenerator:
     """ Lemmatized vocubulary and grammar ngram-based generator """
@@ -41,13 +49,15 @@ class LVGNgramGenerator:
     def _make_models(self, tuples):
         self._word_ids = WordIdDictionary()
 
-        words, lemmas, tags = tuple(map(lambda tokens: list(self._word_ids.add_words_transform(tokens)), zip(*tuples)))
+        words, lemmas, tags = tuple(map(lambda tokens: list(
+            self._word_ids.add_words_transform(tokens)), zip(*tuples)))
         self._words_ngram = NgramModel(words, self._n)
         self._lemmas_ngram = NgramModel(lemmas, self._n)
         self._tags_ngram = NgramModel(tags, self._n)
 
         self._tag_lemmas = ConditionalFreqDist(zip(tags, lemmas))
-        self._tag_lemma_words = ConditionalFreqDist(zip(zip(tags, lemmas), words))
+        self._tag_lemma_words = ConditionalFreqDist(
+            zip(zip(tags, lemmas), words))
 
     def generate(self, n):
         generated_tags = self._tags_ngram.generate(n)
@@ -55,19 +65,19 @@ class LVGNgramGenerator:
         generated_lemmas = []
         for tag in generated_tags:
             choice = self._lemmas_ngram.choose_word(
-                generated_lemmas, lambda lemma: lemma in self._tag_lemmas[tag])
+                generated_lemmas, backoff_limit=2, predicate=lambda lemma: lemma in self._tag_lemmas[tag])
             if choice is None:
                 choice = MLEProbDist(self._tag_lemmas[tag]).generate()
+                print("forced to generate lemma", self._word_ids.transform_id(choice))
             generated_lemmas.append(choice)
 
         generated_words = []
         for (tag, lemma) in zip(generated_tags, generated_lemmas):
             choices = self._words_ngram.backoff_search(
-                generated_words, lambda word: word in self._tag_lemma_words[(tag, lemma)])
-            if choices is not None:
-                generated_words.append(MLEProbDist(choices).generate())
-            else:
-                generated_words.append(MLEProbDist(
-                    self._tag_lemma_words[(tag, lemma)]).generate())
+                generated_words, backoff_limit=2, predicate=lambda word: word in self._tag_lemma_words[(tag, lemma)])
+            if choices is None:
+                choices = self._tag_lemma_words[(tag, lemma)]
+                print("forced to generate words", repr({self._word_ids.transform_id(choice): freq for choice,freq in choices.items()}))
+            generated_words.append(MLEProbDist(choices).generate())
 
         return list(self._word_ids.transform_ids(generated_words))
--- ngram.py
+++ ngram.py
@@ -1,71 +1,135 @@
 from nltk.probability import ConditionalFreqDist, FreqDist, ConditionalProbDist, MLEProbDist
 from nltk.util import ngrams
 
+import sqlite3
+import pickle
+
+def _sqlite_register_pickle(typecls, typename):
+    sqlite3.register_adapter(typecls, pickle.dumps)
+    sqlite3.register_converter(typename, pickle.loads)
+
+_sqlite_register_pickle(tuple, 'tuple')
+_sqlite_register_pickle(dict, 'dict')
+
+class SqliteConditionalFreqDist:
+    def __init__(self, cond_samples=[], database=':memory:'):
+        self._journal = ConditionalFreqDist()
+        self._journal_count = 0
+        
+        conn = sqlite3.connect(database, detect_types=sqlite3.PARSE_DECLTYPES)
+        self._sql = conn.cursor()
+        self._sql.execute('create table cfd(k tuple unique primary key, v dict)')
+
+        self.update(cond_samples)
+
+    def update(self, cond_samples):
+        for (cond, sample) in cond_samples:
+            self._journal[cond][sample] += 1
+
+    def update_one(self, cond, sample):
+        self._journal[cond][sample] += 1
+
+    def commit(self):
+        cfd = self._journal
+        self._journal = ConditionalFreqDist()
+
+        for cond in cfd.keys():
+            olddist = self[cond]
+            cfd[cond] += olddist
+
+        self._sql.executemany('insert or replace into cfd(k, v) values (?, ?)', ((cond, dict(freqdist)) for cond,freqdist in cfd.items()))
+        
+    def commit_replace(self):
+        cfd = self._journal
+        self._journal = ConditionalFreqDist()
+
+        self._sql.executemany('insert or replace into cfd(k, v) values (?, ?)', ((cond, dict(freqdist)) for cond,freqdist in cfd.items()))
+
+    def __getitem__(self, key):
+        self._sql.execute('select v from cfd where k = (?)', (key,))
+        value = self._sql.fetchall()
+        if len(value) > 0:
+            return FreqDist(value[0][0])
+        else:
+            return FreqDist() 
+
+    def __setitem__(self, key, value):
+        value = dict(value)
+        self._sql.execute('insert or replace into cfd(k, v) values (?, ?)', (key, value))
+
+    def __contains__(self, key):
+        self._sql.execute('select v from cfd where k = (?)', (key,))
+        value = self._sql.fetchall()
+        return len(value) > 0
 
 class NgramModel:
 
-    def __init__(self, words, n):
+    def __init__(self, words, n, cfd_class=SqliteConditionalFreqDist):
         self._n = n
 
         if not isinstance(words, list):
             words = list(words)
 
-        self._cpd = ConditionalProbDist(ConditionalFreqDist((ngram[:-1], ngram[-1]) for ngram in ngrams(words, n)), MLEProbDist)
+        cfd = cfd_class()
+        for i in range(n):
+            for ngram in ngrams(words, i + 1):
+                if cfd_class == SqliteConditionalFreqDist:
+                    cfd.update([(ngram[:-1], ngram[-1])])
+                else:
+                    cfd[ngram[:-1]][ngram[-1]] += 1
+        if cfd_class == SqliteConditionalFreqDist:
+            cfd.commit_replace()
+        self._cfd = cfd
 
-        if self._n > 2:
-            self._backoff = NgramModel(words, n - 1)
-        else:
-            self._unigram_pd = MLEProbDist(FreqDist(words))
-
-    def backoff_search(self, context, predicate):
+    def backoff_search(self, context, backoff_limit, predicate):
         context = tuple(context)[1 - self._n:]
-        if context in self:
-            choices = FreqDist({word: freq for (word, freq) in self._cpd[context].freqdist().most_common() if predicate(word)})
+        while True:
+            while not context in self and len(context) >= backoff_limit:
+                context = context[1:]
+            choices = FreqDist({word: freq for (word, freq) in self[
+                               context].most_common() if predicate(word)})
             if len(choices) > 0:
                 return choices
-        if self._n > 2:
-            return self._backoff.backoff_search(context, predicate)
-        else:
-            return None
+            if len(context) < backoff_limit:
+                return None
+            context = context[1:]
 
-    def choose_word(self, context, predicate=None):
-        return self.generate(1, context, predicate)[-1]
+    def choose_word(self, context, backoff_limit=1, predicate=None):
+        return self.generate(1, context, backoff_limit, predicate)[-1]
 
-    def generate(self, num_words, context=[], predicate=None):
+    def generate(self, num_words, context=[], backoff_limit=1, predicate=None):
         text = list(context)
         for i in range(num_words):
             if predicate is None:
-                text.append(self._generate_one(text))
+                text.append(self._generate_one(text, backoff_limit))
             else:
-                text.append(self._generate_one_predicated(text, predicate))
+                text.append(self._generate_one_predicated(
+                    text, backoff_limit, predicate))
         return text
 
-    def _generate_one(self, context):
+    def _generate_one(self, context, backoff_limit):
         context = tuple(context)[1 - self._n:]
+        while not context in self and len(context) >= backoff_limit:
+            context = context[1:]
         if context in self:
-            return self._cpd[context].generate()
-        elif self._n > 2:
-            return self._backoff._generate_one(context)
+            return MLEProbDist(self[context]).generate()
         else:
-            return self._unigram_pd.generate()
+            return None
 
-    def _generate_one_predicated(self, context, predicate):
+    def _generate_one_predicated(self, context, backoff_limit, predicate):
         context = tuple(context)[1 - self._n:]
-        choices = self.backoff_search(context, predicate)
+        choices = self.backoff_search(context, backoff_limit, predicate)
         if choices is not None:
             return MLEProbDist(choices).generate()
         else:
-            # Predicated generate() does not produce a random unigram, as it is
-            # slow to filter the entire unigram list repeatedly. Instead, it
-            # returns None to indicate that it didn't find a valid word
             return None
 
     def __contains__(self, item):
         if not isinstance(item, tuple):
             item = (item,)
-        return item in self._cpd
+        return item in self._cfd
 
     def __getitem__(self, item):
         if not isinstance(item, tuple):
             item = (item,)
-        return self._cpd[item]
+        return self._cfd[item]
