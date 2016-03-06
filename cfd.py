245efdc8f2216c49081c3734911ff163ce0b93cb
Priyank Patel <tocubed@gmail.com>
2016-03-06 14:45:49 -0800

    Improve sqlite memory usage

--- cfd.py
+++ cfd.py
@@ -4,12 +4,70 @@ import sqlite3
 import pickle
 
 
-def _sqlite_register_pickle(typecls, typename):
-    sqlite3.register_adapter(typecls, pickle.dumps)
-    sqlite3.register_converter(typename, pickle.loads)
+def _write_var_uint(uint):
+    output = bytearray()
+    while uint > 127:
+        output.append((uint & 127) | 128)
+        uint >>= 7
+    output.append(uint & 127)
+    return output
 
-_sqlite_register_pickle(tuple, 'tuple')
-_sqlite_register_pickle(dict, 'dict')
+
+def _read_var_uint(bytes):
+    uint = 0
+    for (i, byte) in enumerate(bytes):
+        uint += (byte & 127) << (7 * i)
+        if byte & 128 == 0:
+            break
+    return (uint, i + 1)
+
+
+def _compact_write_ints(iterable):
+    output = bytearray()
+    for i in iterable:
+        output += _write_var_uint(i)
+    return output
+
+
+def _compact_read_ints(bytes):
+    output = []
+    while len(bytes) > 0:
+        uint, read = _read_var_uint(bytes)
+        bytes = bytes[read:]
+        output.append(uint)
+    return output
+
+
+def _compact_inttuple_write(t):
+    return _compact_write_ints(t)
+
+
+def _compact_inttuple_read(bytes):
+    return tuple(_compact_read_ints(bytes))
+
+
+def _compact_intdict_write(d):
+    ints = []
+    for k, v in d.items():
+        ints.extend((k, v))
+    return _compact_write_ints(ints)
+
+
+def _compact_intdict_read(bytes):
+    ints = _compact_read_ints(bytes)
+    return {k: v for (k, v) in zip(ints[::2], ints[1::2])}
+
+sqlite3.register_adapter(tuple, _compact_inttuple_write)
+sqlite3.register_converter("tuple", _compact_inttuple_read)
+sqlite3.register_adapter(dict, _compact_intdict_write)
+sqlite3.register_converter("dict", _compact_intdict_read)
+
+##def _sqlite_register_pickle(typecls, typename):
+##    sqlite3.register_adapter(typecls, pickle.dumps)
+##    sqlite3.register_converter(typename, pickle.loads)
+##
+##_sqlite_register_pickle(tuple, 'tuple')
+##_sqlite_register_pickle(dict, 'dict')
 
 
 class SqliteConditionalFreqDist:
@@ -20,7 +78,7 @@ class SqliteConditionalFreqDist:
         conn = sqlite3.connect(database, detect_types=sqlite3.PARSE_DECLTYPES)
         self._sql = conn.cursor()
         self._sql.execute(
-            'create table cfd(k tuple, v dict)')
+            'create table cfd(k tuple unique primary key, v dict)')
 
         self.update(cond_samples)
 
@@ -48,12 +106,6 @@ class SqliteConditionalFreqDist:
 
         self._sql.executemany('insert or replace into cfd(k, v) values (?, ?)', ((
             cond, dict(freqdist)) for cond, freqdist in cfd.items()))
-
-        self._commit_finalize()
-
-    def _commit_finalize(self):
-        self._sql.execute(
-            'create unique index kidx on cfd(k)')
 
     def __getitem__(self, key):
         self._sql.execute('select v from cfd where k = (?)', (key,))
